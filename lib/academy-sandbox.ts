b9ba208d9516bbfb986b46678e0c36ac43ccaacb
Vadik Goel <vadikgoel1@gmail.com>
2026-09-16 16:01:47 +0530

    fix: use native .NET runtime entrypoint

--- lib/academy-sandbox.ts
+++ lib/academy-sandbox.ts
@@ -60,7 +60,7 @@ export function buildSandboxArgs(
 
   return phase === 'compile'
     ? [...common, 'dotnet', 'build', '/input/AcademyRunner.csproj', '--nologo', '--ignore-failed-sources', '-o', '/output', '-p:BaseIntermediateOutputPath=/tmp/obj/', '-p:MSBuildProjectExtensionsPath=/tmp/obj/', '-p:MSBuildEnableWorkloadResolver=false']
-    : [...common, 'dotnet', '/output/AcademyRunner.dll']
+    : [...common, '/output/AcademyRunner.dll']
 }
 
 export function sandboxSecurityContract(policy: AcademyExecutionPolicy) {
